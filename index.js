1a7bac5cd0846cbc3b1566149822f121d9061e8e
allenMe3 <756440239@qq.com>
2020-05-22 22:16:11 +0800

    Signed-off-by: allenMe3 <756440239@qq.com>

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var script=document.createElement("script"); 
+script.type="text/javascript"; 
+script.src="jquery.js"; 
 function qq() {
     alert("本人手机：18278454149  QQ：756440239")
 }
+// function jinru(){
+//     document.getElementsByClassName("center").style.
+// }
+// $(document).ready(function(){
+//     $(".enter").hide()
+// })
+// $(document).ready(function(){
+//     $(".center").click(function(){
+//         $(".center").hide();
+//         $("body").css("background-image","none");
+//         $(".enter").show();
+//     })
+// });
