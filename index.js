b0bd29827b4d2e51790d80f3f14f6d088d0fe1a3
allenMe3 <756440239@qq.com>
2020-05-24 17:37:35 +0800

    Signed-off-by: allenMe3 <756440239@qq.com>

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ script.src="jquery.js";
 function qq() {
     alert("本人手机：18278454149  QQ：756440239")
 }
-// function jinru(){
-//     document.getElementsByClassName("center").style.
-// }
-// $(document).ready(function(){
-//     $(".enter").hide()
-// })
-// $(document).ready(function(){
-//     $(".center").click(function(){
-//         $(".center").hide();
-//         $("body").css("background-image","none");
-//         $(".enter").show();
-//     })
-// });
+$(document).ready(function(){
+    $(".enter").hide()
+})
+$(document).ready(function(){
+    $(".center").click(function(){
+        $(".center").hide();
+        $("body").css("background-image","none");
+        $(".enter").show();
+        $(".dian").addClass("dianj")
+    })
+});
